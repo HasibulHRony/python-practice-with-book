254c992ad0029dcd1152b58c900a1028dfff9010
HasibulHRony <hasibulhrony7@gmail.com>
2026-05-11 14:18:52 +0600

    checking year leap year or not.

--- Conditional-logic/learYear.py
+++ Conditional-logic/learYear.py
@@ -1,5 +1,18 @@
-year = 1300
+year = int(input("Write year:  "))
 
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Yes!", year, "is a leap year!", sep=" ")
+        else:
+            print("No! The year you enterd", year, "is not leap year!", sep=" ")
+    else:
+        print("Yes!", year, "is a leap year!", sep=" ")    
+else:
+    print("No! The year you enterd", year, "is not leap year!", sep=" ")
+
+
+# another way
 if year % 100 != 0 and year % 4 == 0:
     print(year, "is a leap year!")
 elif year % 100 == 0 and year % 400 == 0:
