e402c42db3f2091975fb256cdc0a1d268e4c99c8
HasibulHRony <hasibulhrony7@gmail.com>
2026-05-15 10:10:03 +0600

    drawing circle through square by turtle using function.

---
+++ function/functionIntro.py
@@ -0,0 +1,9 @@
+# function of summation of two numbers
+
+def sumOfNums(num1, num2):
+    return num1 + num2
+
+number1 = 5
+number2 = 10
+result = sumOfNums(number1, number2)
+print("sum of number1 and number2 is:", result)
